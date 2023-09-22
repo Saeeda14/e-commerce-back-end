34b00e43f50416d3424c8380278df19e7b9aee5d
Saeeda <saeedabatool39@gmail.com>
2023-09-22 19:45:57 -0400

    added columns for producttag

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -7,6 +7,26 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product', // This references the 'product' model
+        key: 'id', // This references the 'id' column in the 'product' model
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag', // This references the 'tag' model
+        key: 'id', // This references the 'id' column in the 'tag' model
+      },
+    },
   },
   {
     sequelize,
